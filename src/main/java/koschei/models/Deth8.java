ea35b2d021909859f54eeaf8a960a1cd06a58233
Evgen12412 <evgeny.krd2016@yandex.ru>
2023-02-17 11:55:50 +0300

    изменил убрав аннотацию

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -6,10 +6,4 @@ import org.springframework.stereotype.Component;
 @Component
 public class Deth8 {
 
-
-
-    @Override
-    public String toString() {
-        return ", смерть Кощея на игле :(";
-    }
 }
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,13 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    @Autowired
     private Egg6 egg6;
-
-    public Duck5(Egg6 egg6) {
-        this.egg6 = egg6;
-    }
-
-
 
     @Override
     public String toString() {
